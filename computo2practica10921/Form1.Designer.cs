2f3b882e211a7e07d6a42a6c0349d8b7986cd58d
Calix1402 <88732337+Calix1402@users.noreply.github.com>
2021-09-09 15:12:24 -0600

    practica

---
+++ computo2practica10921/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace computo2practica10921
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFill = new System.Windows.Forms.Button();
+            this.dgData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(87, 69);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(150, 126);
+            this.btnFill.TabIndex = 0;
+            this.btnFill.Text = "GetStuden";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgData
+            // 
+            this.dgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgData.Location = new System.Drawing.Point(346, 69);
+            this.dgData.Name = "dgData";
+            this.dgData.RowHeadersWidth = 62;
+            this.dgData.RowTemplate.Height = 28;
+            this.dgData.Size = new System.Drawing.Size(974, 452);
+            this.dgData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1332, 627);
+            this.Controls.Add(this.dgData);
+            this.Controls.Add(this.btnFill);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFill;
+        private System.Windows.Forms.DataGridView dgData;
+    }
+}
+
---
+++ computo2practica10921/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computo2practica10921
+{
+    class Student
+    {
+        
+        public int idStudent { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string genero { get; set; }
+
+        //crear coleccion de datos 
+        public static List<Student> GetStudents()
+        {
+
+            List<Student> Students = new List<Student>
+            {
+               new Student {idStudent = 1001, firstName ="Angie",lastName ="Segovia", age = 17, genero = "femenino" },
+               new Student{idStudent = 1002, firstName ="Ashlee", lastName ="Calix", age=17, genero = "Femenino"},
+               new Student {idStudent = 1003, firstName ="Flor", lastName ="Torres", age = 34, genero = "Femenino" },
+               new Student { idStudent = 1004, firstName ="walter", lastName ="Calix", age = 43, genero = "Masculino" },
+               new Student {idStudent = 1005, firstName ="Alexis", lastName ="Calix", age = 18, genero = "Masculino" },
+               new Student {idStudent = 1006, firstName ="Carmen", lastName ="Rodriguez", age = 20, genero = "Femenino" },
+               new Student {idStudent = 1007, firstName ="Carlos", lastName = " Rojas", age = 25, genero = " Masculino"},
+            };
+
+            //valor de reotorno sera la lista del estudiante 
+
+            return Students;
+
+        }
+    }
+}
+
+
+    
+
